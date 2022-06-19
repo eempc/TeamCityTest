f2bc2a462437521466a9c6cfd855fe9f492901ac
Evelyn <evc776@gmail.com>
2022-06-19 09:50:41 +0100

    removed sout

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Meow some more");
-        System.out.println("Evelyn's test of TeamCity");
+
     }
 }
