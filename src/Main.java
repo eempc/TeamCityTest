909b73c96fa1f78f951500ea2edcabe655d0033d
Evelyn <evc776@gmail.com>
2022-06-19 09:48:37 +0100

    new line

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Meow");
+        System.out.println("Meow some more");
+        System.out.println("Evelyn's test of TeamCity");
     }
 }
